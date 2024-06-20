bfb1138775cee9ecb723c8cbabd2d54400a3862e
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2024-06-20 14:09:43 -0700

    linting fixes

--- dave_demos/launch/model_in_empty_world.launch.py
+++ dave_demos/launch/model_in_empty_world.launch.py
@@ -1,9 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import (
-    LaunchConfiguration,
-    PathJoinSubstitution
-    )
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
