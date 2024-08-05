6c3b0b14f10ed722c102eff150ec1d934de89eda
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2024-08-04 18:52:30 -0700

    Added a custom ros-gz bridge for DVL messages

---
+++ gazebo/dave_ros_gz_plugins/include/dave_ros_gz_plugins/DVLBridge.hh
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2024 Rakesh Vivekanandan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef DAVE_ROS_GZ_PLUGINS__DVLBRIDGE_HH_
+#define DAVE_ROS_GZ_PLUGINS__DVLBRIDGE_HH_
+
+#include <memory>
+
+#include <gz/sim/System.hh>
+#include <gz/msgs/dvl_velocity_tracking.pb.h>
+#include <rclcpp/rclcpp.hpp>
+#include "dave_interfaces/msg/dvl.hpp"
+#include "dave_interfaces/msg/dvl_beam.hpp"
+
+
+namespace dave_ros_gz_plugins
+
+{
+class DVLBridge : public gz::sim::System,
+                        public gz::sim::ISystemConfigure,
+                        public gz::sim::ISystemPostUpdate
+{
+public:
+  DVLBridge();
+  ~DVLBridge() override = default;
+
+  void Configure(
+    const gz::sim::Entity & entity, const std::shared_ptr<const sdf::Element> & sdf,
+    gz::sim::EntityComponentManager & ecm, gz::sim::EventManager & eventMgr) override;
+
+  void PostUpdate(
+    const gz::sim::UpdateInfo & info, const gz::sim::EntityComponentManager & ecm) override;
+
+  void receiveGazeboCallback(const gz::msgs::DVLVelocityTracking & dvl_velocity_tracking);
+
+private:
+  std::shared_ptr<rclcpp::Node> ros_node_;
+
+  struct PrivateData;
+  enum DVLType{
+    DVL_TYPE_UNSPECIFIED = 0,
+    DVL_TYPE_PISTON = 1,
+    DVL_TYPE_PHASED_ARRAY = 2
+    };
+  enum DVLTargetType{
+    DVL_TARGET_UNSPECIFIED = 0,
+    DVL_TARGET_BOTTOM = 1,
+    DVL_TARGET_WATER = 2
+    };
+  enum ReferenceType{
+    DVL_REFERENCE_UNSPECIFIED = 0,
+    DVL_REFERENCE_EARTH = 1,
+    DVL_REFERENCE_SHIP = 2
+    };
+  std::unique_ptr<PrivateData> dataPtr;
+};
+}  // namespace dave_ros_gz_plugins
+
+#endif  // DAVE_ROS_GZ_PLUGINS__DVLBRIDGE_HH_
---
+++ gazebo/dave_ros_gz_plugins/src/DVLBridge.cc
@@ -0,0 +1,182 @@
+/*
+ * Copyright (C) 2024 Rakesh Vivekanandan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+
+#include <gz/common/Console.hh>
+#include <rclcpp/rclcpp.hpp>
+#include "gz/plugin/Register.hh"
+#include <gz/transport/Node.hh>
+
+
+#include "dave_ros_gz_plugins/DVLBridge.hh"
+
+GZ_ADD_PLUGIN(
+  dave_ros_gz_plugins::DVLBridge, gz::sim::System,
+  dave_ros_gz_plugins::DVLBridge::ISystemConfigure,
+  dave_ros_gz_plugins::DVLBridge::ISystemPostUpdate)
+
+namespace dave_ros_gz_plugins
+{
+
+struct DVLBridge::PrivateData
+{
+  // Add any private data members here.
+  gz::transport::Node m_gzNode;
+  rclcpp::Publisher<dave_interfaces::msg::DVL>::SharedPtr dvl_pub;
+};
+
+DVLBridge::DVLBridge() : dataPtr(std::make_unique<PrivateData>()) {}
+
+void DVLBridge::Configure(
+  const gz::sim::Entity & _entity, const std::shared_ptr<const sdf::Element> & _sdf,
+  gz::sim::EntityComponentManager & _ecm, gz::sim::EventManager & _eventManager)
+{
+  gzdbg << "dave_ros_gz_plugins::DVLBridge::Configure on entity: " << _entity << std::endl;
+
+  if (!rclcpp::ok())
+  {
+    rclcpp::init(0, nullptr);
+  }
+
+  this->ros_node_ = std::make_shared<rclcpp::Node>("dvl_bridge_node");
+
+}
+
+void DVLBridge::receiveGazeboCallback(const gz::msgs::DVLVelocityTracking & msg)
+{
+  gzmsg << "dave_ros_gz_plugins::DVLBridge::receiveGazeboCallback" << std::endl;
+
+  auto dvl_msg = dave_interfaces::msg::DVL();
+  dvl_msg.header.stamp.sec = msg.header().stamp().sec();
+  dvl_msg.header.stamp.nanosec = msg.header().stamp().nsec();
+
+  std::string dvl_type;
+  std::string target_type;
+  std::string reference_type;
+
+  switch (msg.type())
+  {
+    case DVLType::DVL_TYPE_UNSPECIFIED:
+      dvl_type = "DVL_TYPE_UNSPECIFIED";
+      break;
+    case DVLType::DVL_TYPE_PISTON:
+      dvl_type = "DVL_TYPE_PISTON";
+      break;
+    case DVLType::DVL_TYPE_PHASED_ARRAY:
+      dvl_type = "DVL_TYPE_PHASED_ARRAY";
+      break;
+    default:
+      dvl_type = "DVL_TYPE_UNSPECIFIED";
+  }
+
+  switch (msg.target().type())
+  {
+    case DVLTargetType::DVL_TARGET_UNSPECIFIED:
+      target_type = "DVL_TARGET_UNSPECIFIED";
+      break;
+    case DVLTargetType::DVL_TARGET_BOTTOM:
+      target_type = "DVL_TARGET_BOTTOM";
+      break;
+    case DVLTargetType::DVL_TARGET_WATER:
+      target_type = "DVL_TARGET_WATER";
+      break;
+    default:
+      target_type = "DVL_TARGET_UNSPECIFIED";
+  }
+
+  dvl_msg.type = dvl_type;
+
+  dvl_msg.target.type = target_type;
+  dvl_msg.target.range = msg.target().range().mean();
+
+  dvl_msg.velocity.twist.linear.x = msg.velocity().mean().x();
+  dvl_msg.velocity.twist.linear.y = msg.velocity().mean().y();
+  dvl_msg.velocity.twist.linear.z = msg.velocity().mean().z();
+
+  size_t covariance_size = msg.velocity().covariance().size();
+
+  for (size_t i = 0; i < covariance_size; i++)
+  {
+    dvl_msg.velocity.covariance[i] = msg.velocity().covariance()[i];
+  }
+
+  size_t beam_size = msg.beams().size();
+
+  for (size_t i = 0; i < beam_size; i++)
+  {
+    auto beam = dave_interfaces::msg::DVLBeam();
+    beam.id = msg.beams()[i].id();
+
+    switch (msg.beams()[i].velocity().reference())
+    {
+      case ReferenceType::DVL_REFERENCE_UNSPECIFIED:
+        reference_type = "DVL_REFERENCE_UNSPECIFIED";
+        break;
+      case ReferenceType::DVL_REFERENCE_EARTH:
+        reference_type = "DVL_REFERENCE_EARTH";
+        break;
+      case ReferenceType::DVL_REFERENCE_SHIP:
+        reference_type = "DVL_REFERENCE_SHIP";
+        break;
+      default:
+        reference_type = "DVL_REFERENCE_UNSPECIFIED";
+    }
+
+    beam.reference = reference_type;
+    beam.range = msg.beams()[i].range().mean();
+    beam.locked = msg.beams()[i].locked();
+
+    beam.velocity.twist.linear.x = msg.beams()[i].velocity().mean().x();
+    beam.velocity.twist.linear.y = msg.beams()[i].velocity().mean().y();
+    beam.velocity.twist.linear.z = msg.beams()[i].velocity().mean().z();
+
+    size_t beam_covariance_size = msg.beams()[i].velocity().covariance().size();
+
+    for (size_t j = 0; j < beam_covariance_size; j++)
+    {
+      beam.velocity.covariance[j] = msg.beams()[i].velocity().covariance()[j];
+    }
+
+    dvl_msg.beams.push_back(beam);
+  }
+
+  this->dataPtr->dvl_pub->publish(dvl_msg);
+}
+
+void DVLBridge::PostUpdate(
+  const gz::sim::UpdateInfo & _info, const gz::sim::EntityComponentManager & _ecm)
+{
+  if (!_info.paused)
+  {
+    rclcpp::spin_some(this->ros_node_);
+
+    // Gazebo subscriber
+    std::string dvl_topic = "/dvl/velocity";
+
+    std::function<void(const gz::msgs::DVLVelocityTracking &)> callback =
+      std::bind(&DVLBridge::receiveGazeboCallback, this, std::placeholders::_1);
+
+    this->dataPtr->m_gzNode.Subscribe(dvl_topic, callback);
+
+    // ROS2 publisher
+    this->dataPtr->dvl_pub = this->ros_node_->create_publisher<dave_interfaces::msg::DVL>(
+      dvl_topic, 1);
+
+  }
+}
+
+}  // namespace dave_ros_gz_plugins
